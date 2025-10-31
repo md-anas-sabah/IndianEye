2e96a71f42bb870cee7aebdcfa39114681bae68d
md-anas-sabah <mdanassabah@gmail.com>
2025-10-31 16:29:26 +0530

    feat: added lines

--- .next/dev/static/chunks/app_layout_tsx_3873bb71._.js
+++ .next/dev/static/chunks/app_layout_tsx_3873bb71._.js
@@ -2,7 +2,8 @@
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
   "static/chunks/[root-of-the-server]__99176052._.css",
-  "static/chunks/node_modules_next_dist_279f65b1._.js"
+  "static/chunks/node_modules_b827adc1._.js",
+  "static/chunks/components_header_tsx_59596cc5._.js"
 ],
     source: "dynamic"
 });
