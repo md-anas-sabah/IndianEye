03533b3345809072ab28cf0c120e78cebb71b133
md-anas-sabah <mdanassabah@gmail.com>
2025-10-31 21:03:39 +0530

    feat: added right side content

--- app/page.tsx
+++ app/page.tsx
@@ -2,33 +2,34 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div
-      className="flex flex-col md:flex-row w-full min-h-[calc(100vh-74px)] bg-cover -mt-1.5 bg-center bg-no-repeat justify-around items-center gap-8 px-8"
-      style={{ backgroundImage: "url('/landing/bg.png')" }}
-    >
+    <div className="flex flex-col md:flex-row w-full min-h-[calc(100vh-74px)] bg-cover -mt-1.5 bg-center bg-no-repeat justify-around items-center gap-8 px-8 bg-[url('/landing/bg.png')]">
       {/* Left Container */}
-      <div
-        className="rounded-[40px]"
-        style={{
-          width: "532px",
-          height: "287px",
-          background: "#00000066",
-          backdropFilter: "blur(28px)",
-          opacity: 1,
-        }}
-      ></div>
+      <div className="rounded-[40px] flex flex-col justify-center items-start p-8 gap-4 w-[532px] h-[287px] bg-black/40 backdrop-blur-[28px]">
+        <h2 className="font-(family-name:--font-albert-sans) font-semibold text-[28px] leading-[100%] text-white">
+          Reinforcing National Defence with Next-Gen Tech
+        </h2>
+        <p className="font-(family-name:--font-albert-sans) font-normal text-base leading-[150%] text-[#DADADA]">
+          Cutting-edge unmanned aerial tech built for surveillance, security,
+          and tactical advantage. Engineered for precision, reliability, and
+          mission success.
+        </p>
+        <button className="font-(family-name:--font-albert-sans) font-normal text-base leading-[100%] text-white bg-[#259351] px-6 py-2 rounded-full hover:opacity-90 transition-opacity">
+          Visit Site
+        </button>
+      </div>
 
       {/* Right Container */}
-      <div
-        className="rounded-[40px]"
-        style={{
-          width: "532px",
-          height: "287px",
-          background: "#00000066",
-          backdropFilter: "blur(28px)",
-          opacity: 1,
-        }}
-      ></div>
+      <div className="rounded-[40px] flex flex-col justify-start items-start p-8 gap-4 w-[532px] h-[287px] bg-black/40 backdrop-blur-[28px]">
+        <h2 className="font-(family-name:--font-albert-sans) font-semibold text-[28px] leading-[100%] text-white">
+          State of the art training excellence for nation building
+        </h2>
+        <p className="font-(family-name:--font-albert-sans) font-normal text-base leading-[150%] text-[#DADADA]">
+          Indianeye institute of homeland security
+        </p>
+        <button className="font-(family-name:--font-albert-sans) font-normal text-base leading-[100%] text-white bg-[#259351] px-6 py-2 rounded-full hover:opacity-90 transition-opacity">
+          Visit Site
+        </button>
+      </div>
     </div>
   );
 }
